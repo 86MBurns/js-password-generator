5130aff5dca8f1f72cfd2e0efdd6871aa40e66b4
Matt Burns <matt.burns01@gmail.com>
2022-07-04 06:27:01 -0700

    got frustrated and started over. created the character strings and password length.

--- script.js
+++ script.js
@@ -1,69 +1,48 @@
-var generateBtn= document.querySelectorAll("generate");
+// what data dose my app need to work?
 
-function writePassword(){
-  var password = generatePassword();
-  var passwordText= document.querySelector("password");
-  passwordText.value = password;
-}
+// what sorts of actions need to complete?
 
-// soring all needed characters and variables
-var capitalLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCaseLetters= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+
+
+function generatePassword() {
+    var numbers = "0123456789".split;
+
+var upperCaseLetters= "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+
 var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",",","|",";",":","'","<",".",">","/","?"];
 
-// what sorts of action need to comlepet.
-
-//generat password
-function generatePassword() {
-
-  var passwordlength;
-  var useCapital;
-  var useLowerCase;
-  var useNunbers;
-  var useSpecialCharaters;
-  //promt the user for their passwordlength
-  passwordlength = window.prompt("Request password length");
-  //validate password length for number of characters is 8 to 128
+var useableCharacters = [];
+    // prompt user for passwordLength and store it in a variable
   
-  //If passwordLength is > than or equal to 8 and passwordLength less that or equal to 128
+   
 
-  //If passwordLength < less that 8 OR it greater than 128
-if( length >= 8 && length <= 128 ) {
-
-  useLowerCase = window.confirm( "Do you want to use lower case letters?");
-
-  useCapital = window.confirm( "Do you want to use capital letters?");
-
-  useNunbers = window.confirm("Do you want to use numbers?");
-
-  useSpecialCharaters = window.confirm("Do you want to use special characters?");
-
-  if(useLowerCase || useCapital || useNunbers || useSpecialCharaters) {
-
-    if (useLowerCase){
-      
+var passwordLength = prompt("Choose number of characters from 8 to 128.");
+    //validate the password length and make sure the its equal to 8 up to 128
+    if (passwordLength >= 8 && passwordLength <= 128){
+        return "Please choose a number from 8 up to 128.";
+    } else if (isNaN(passwordLength)) {
+        passwordLength = prompt("Please us a valid number")
     }
-  }
-  //end fuction early
-  return;
-}
-
+    //prompt the user for special character and store in a variable
+     
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
 }
 
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn= document.querySelectorAll("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword(){
+    var password = generatePassword();
+    var passwordText= document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
-}
+  }
+  //add event listenter to generate button
+  generateBtn.addEventListener("click", writePassword);
+//variables for all characters
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
