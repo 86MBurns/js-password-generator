54901a76b9fb4b82ef0e83dc182838ec62be38c3
Matt Burns <matt.burns01@gmail.com>
2022-07-05 12:37:13 -0700

    tryed to trubble shoot NAN with no luck

--- script.js
+++ script.js
@@ -1,59 +1,6 @@
 // what data dose my app need to work?
-
-// what sorts of actions need to complete?
-
-
-
-function generatePassword() {
-var numbers = ["0123456789"].split;
-
-var upperCaseLetters= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"].split("");
-
-var lowercaseLetters = ["abcdefghijklmnopqrstuvwxyz"].split("");
-
-var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",",","|",";",":","'","<",".",">","/","?"];
-
-var useableCharacters = [];
-    // prompt user for passwordLength and store it in a variable
-var passwordLength;  
-var useNumbers; 
-var useUpperCase;
-var useLowerCase;
-var useSpecial;  
-
-passwordLength = window.prompt("Choose number of characters from 8 to 128.");
-    //validate the password length and make sure the its equal to 8 up to 128
-    if (passwordLength >= 8 && passwordLength <= 128){
-        return ;
-    }
-    useNumbers = confirm ("Do you want to use numbers?");
-    useUpperCase = confirm ("Do you want to use capital letters?");
-    useLowerCase = confirm ("Do you want to use lowercase leters?");
-    useSpecial = confirm ("Do you want to use special characters?");
-    //prompt the user for special character and store in a variable
-    if (!useNumbers || !useUpperCase || !useLowerCase || !useSpecial){
-        return "Select at least one type of characters.";
-    };
-
-    if (useNumbers) {
-        useableCharacters = useableCharacters.concat(numbers);
-    }
-    if (useUpperCase) {
-        useableCharacters = useableCharacters.concat(upperCaseLetters);
-    }
-    if (useLowerCase) {
-        useableCharacters = useableCharacters.concat(lowercaseLetters);
-    }
-    if (useSpecial) {
-        useableCharacters = useableCharacters.concat(specialCharacters);
-    }
-
-   
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
-}
-
 // Assignment Code
-var generateBtn= document.querySelectorAll("#generate");
+var generateBtn = document.querySelectorAll("#generate");
 
 function writePassword(){
     var password = generatePassword();
@@ -63,8 +10,90 @@ function writePassword(){
 
   }
   //add event listenter to generate button
-  generateBtn.addEventListener("click", writePassword);
-//variables for all characters
+  generate.addEventListener("click", writePassword);
+
+// what sorts of actions need to complete?
+
+function generatePassword() {
+    //variables for all characters
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+
+var upperCaseLetters= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var specialCharacters = ["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",",","|",";",":","'","<",".",">","/","?"];
+
+var useableCharacters = "" ;
+
+ var finalPassword = (passwordLength * useableCharacters) ;
+
+    // prompt user for passwordLength and store it in a variable
+var passwordLength;  
+var useNumbers; 
+var useUpperCase;
+var useLowerCase;
+var useSpecial;  
+
+
+passwordLength = window.prompt("Choose number of characters from 8 to 128.");
+    //validate the password length and make sure the its equal to 8 up to 128
+    if (passwordLength >= 8 && passwordLength <= 128 ){
+     
+    } 
+        
+    useNumbers = confirm ("Do you want to use numbers?");
+    if (useNumbers){
+        alert("your password will use numbers.");
+    }
+    useUpperCase = confirm ("Do you want to use capital letters?");
+    if (useUpperCase){
+        alert("your password will use uppercase letter.");
+    }
+    useLowerCase = confirm ("Do you want to use lowercase leters?");
+    if (useLowerCase){
+        alert("your password will use lowercase leters.");
+    }
+    useSpecial = confirm ("Do you want to use special characters?");
+    if (useSpecial){
+        alert("your password will use special characters.");
+    }
+    if (useNumbers || useUpperCase || useLowerCase || useSpecial){
+      
+    };
+
+    if (useNumbers == true) {
+        useableCharacters = useableCharacters.concat(numbers);
+        
+    }
+    if (useUpperCase == true) {
+        useableCharacters = useableCharacters.concat(upperCaseLetters);
+        
+    }
+    if (useLowerCase == true) {
+        useableCharacters = useableCharacters.concat(lowercaseLetters);
+        
+    }
+    if (useSpecial == true) {
+        useableCharacters = useableCharacters.concat(specialCharacters);
+        
+    }
+    
+
+
+    for (var i = 0; i <= passwordLength; i++){
+    useableCharacters [Math.floor (Math.random() * useableCharacters.finalPassword)];
+       
+
+   return finalPassword ;
+   }
+
+   
+    //prompt the user for special character and store in a variable
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+}
+
+
 
 
 
